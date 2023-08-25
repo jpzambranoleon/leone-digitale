0e58b931cc1e42b0b313b7f0b2bf915e2ab9b83e
Jean-Paul <jpzambrano.leon@gmail.com>
2023-08-25 14:17:00 -0500

    modified file

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,45 +1,44 @@
 import { Link } from "react-router-dom";
+import Logo from "../assets/images/bg-blob.png";
 
 export default function Navbar() {
   return (
     <header className="header">
       <div className="container">
         <div className="overlay" data-overlay></div>
-        <nav>
-          <div className="navbar">
-            <div className="navbar-top">
-              <Link to="/" onClick={() => window.scrollTo(0, 0)}>
-                <h1 className="logo">Leone Digitale</h1>
-              </Link>
-            </div>
-            <div className="navbar-right">
-              <ul className="navbar__links">
-                <li>
-                  <Link className="home-link" to="/">
-                    Home
-                  </Link>
-                </li>
-                <li>
-                  <Link className="about-link" to="/about">
-                    About
-                  </Link>
-                </li>
-                <li>
-                  <Link className="team-link" to="/team">
-                    Our Team
-                  </Link>
-                </li>
-                <li>
-                  <Link className="contact-link" to="/contact">
-                    Contact
-                  </Link>
-                </li>
-              </ul>
-              <div className="navbar__button">
-                <Link className="navbar__button__get-quote" to="/">
-                  Get a quote
+        <nav className="navbar">
+          <div className="navbar-left">
+            <Link
+              to="/"
+              onClick={() => window.scrollTo(0, 0)}
+              className="navbar-left__logo"
+            >
+              <h1 className="logo-text">Leone Digitale</h1>
+              <img src={Logo} className="logo-img" />
+            </Link>
+          </div>
+          <div className="navbar-right">
+            <ul className="navbar__links">
+              <li>
+                <Link className="home-link" to="/">
+                  Home
                 </Link>
-              </div>
+              </li>
+              <li>
+                <Link className="team-link" to="/team">
+                  Our Team
+                </Link>
+              </li>
+              <li>
+                <Link className="contact-link" to="/contact">
+                  Contact
+                </Link>
+              </li>
+            </ul>
+            <div className="navbar__button">
+              <Link className="navbar__button__get-quote" to="/">
+                Get a quote
+              </Link>
             </div>
           </div>
         </nav>
